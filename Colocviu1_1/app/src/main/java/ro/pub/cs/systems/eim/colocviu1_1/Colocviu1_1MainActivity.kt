e253229f3cbcadd63ae6c8b79411afaa9b45f024
LepinzeanStefan <stefan.lepinzean@gmail.com>
2025-11-06 09:06:58 +0200

    B.1 B.2

--- Colocviu1_1/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/Colocviu1_1MainActivity.kt
+++ Colocviu1_1/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/Colocviu1_1MainActivity.kt
@@ -1,7 +1,14 @@
 package ro.pub.cs.systems.eim.colocviu1_1
 
+import android.R.id.input
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import ro.pub.cs.systems.eim.colocviu1_1.Constants.BTNS_PRESSED
+import ro.pub.cs.systems.eim.colocviu1_1.Constants.MAIN_ACTIVIVTY_TAG
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,8 +21,66 @@ import androidx.compose.ui.tooling.preview.Preview
 import ro.pub.cs.systems.eim.colocviu1_1.ui.theme.Colocviu1_1Theme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var inputText: TextView
+    private var btnsPressed = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_colocviu1_1_main)
+
+        inputText = findViewById(R.id.text_pressed)
+
+        val buttonNorth = findViewById<Button>(R.id.btn_north)
+        buttonNorth.setOnClickListener {
+            btnsPressed++
+            if (inputText.text.isEmpty())
+                inputText.append("north")
+            else
+                inputText.append(", north")
+        }
+
+        val buttonSouth = findViewById<Button>(R.id.btn_south)
+        buttonSouth.setOnClickListener {
+            btnsPressed++
+            if (inputText.text.isEmpty())
+                inputText.append("south")
+            else
+                inputText.append(", south")
+        }
+
+        val buttonEast = findViewById<Button>(R.id.btn_east)
+        buttonEast.setOnClickListener {
+            btnsPressed++
+            if (inputText.text.isEmpty())
+                inputText.append("east")
+            else
+                inputText.append(", east")
+        }
+
+        val buttonWest = findViewById<Button>(R.id.btn_west)
+        buttonWest.setOnClickListener {
+            btnsPressed++
+            if (inputText.text.isEmpty())
+                inputText.append("west")
+            else
+                inputText.append(", west")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.d(MAIN_ACTIVIVTY_TAG, "save : btnsPressed=$btnsPressed")
+
+        outState.putInt(BTNS_PRESSED, btnsPressed)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.containsKey(BTNS_PRESSED)) {
+            btnsPressed = savedInstanceState.getInt(BTNS_PRESSED)
+        }
+
+        Log.d(MAIN_ACTIVIVTY_TAG, "restore : btnsPressed=$btnsPressed")
     }
 }
---
+++ Colocviu1_1/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/Constants.kt
@@ -0,0 +1,7 @@
+package ro.pub.cs.systems.eim.colocviu1_1
+
+object Constants {
+    const val BTNS_PRESSED = "btnsPressed"
+
+    const val MAIN_ACTIVIVTY_TAG = "[Main activity thread]"
+}
